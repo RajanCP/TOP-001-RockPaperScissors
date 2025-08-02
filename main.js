c7d5cbfb1fa1f3ccc1dd6eab76a29326630b00d8
RajanCP <rjnptl2000@gmail.com>
2025-08-03 00:01:46 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const getComputerChoice = function () {
+    var choice = "";
+    var random = Math.ceil(Math.random() * 3);
+    console.log(random);
+    if (random === 1) {
+        choice = "Rock";
+    }
+    else if (random === 2) {
+        choice = "Paper";
+    }
+    else {
+        choice = "Scissors";
+    }
+    return choice;
+};
+
+console.log(getComputerChoice());
+
+const getHumanChoice = function(){
+
+ const getHumanChoice = function() {
+    var userChoice = "";
+    const userPrompt = prompt("Choose Rock, Paper, or Scissors");
+
+    if (userPrompt === "Rock" || userPrompt === "rock") {
+        userChoice = "Rock";
+    } else if (userPrompt === "Paper" || userPrompt === "paper") {
+        userChoice = "Paper";
+    } else if (userPrompt === "Scissors" || userPrompt === "scissors") {
+        userChoice = "Scissors";
+    } else {
+        alert("Invalid choice. Please choose Rock, Paper, or Scissors.");
+        return getHumanChoice(); 
+    }
+
+    return userChoice;
+};
+    }
+
+console.log(getHumanChoice())
